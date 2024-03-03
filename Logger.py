b80b14750ff3a6d515cd3b1c7dbe731d256ca315
groKo12 <129460086+groKo12@users.noreply.github.com>
2024-03-02 18:09:04 -0600

    Updated Logger to include a timestamp with entries

--- Logger.py
+++ Logger.py
@@ -1,16 +1,22 @@
-import os
-from datetime import datetime
+import os, time
+
 # General purpose function to write data to a file
-def write_to_log(file_path, string, timestamp=datetime.now()):
+def write_to_log(file_path, string, def_format="%Y-%m-%d %H:%M:%S"):
     # strip file name off
     dir = os.path.dirname(file_path)
-    print(dir)
 
     # Create dir if it doesn't exist
     os.makedirs(dir, exist_ok=True)
 
+    # Grab Current time
+    current_timestamp = time.time()
+    formatted_time = time.strftime(def_format, time.gmtime(current_timestamp))
+
+
+    # Write timestamped string to specified file
     with open(file_path, 'a') as f:
-        f.write(timestamp + string)
+        time_string = formatted_time + " " + string
+        f.write(time_string)
     f.close()
 
 
